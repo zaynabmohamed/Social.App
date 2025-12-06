f40e5c49e67beb6f52daaf1e7f84aa5c7c8fdaa2
zaynabmohamed <zm2438320@gmail.com>
2025-12-06 17:02:52 +0200

    edite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-     tailwindcss()],
-  server:{
-    open:true
-  }
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    open: true,
+  },
+  build: {
+    outDir: 'dist', // مهم جداً للـ Vercel
+  },
+});
